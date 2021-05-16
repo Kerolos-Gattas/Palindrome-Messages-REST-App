df77a22f8ba52e7497698a2c77149e03f53466d6
Kerolos-Gattas <kerolos_gattas@hotmail.com>
2021-05-16 10:19:15 -0400

    Add app start settings

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express = require("express");
+import ConversationDataManager from "./data managment services/conversationDataManager";
 
 // Our Express APP config
 const app = express();
-app.set("port", process.env.PORT || 3000);
-
-// API Endpoints
-app.get("/", (req, res) => {
-    res.send("Hi");
+app.use(express.json());
+const port = process.env.PORT || 3000;
+app.set("port", port);
+const runningMessage = `Server running at http://localhost:${port}`;
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.status(200).send(runningMessage)
 });
+
+// TODO add winston logger
+// TODO Test simultaneous add, update and delete
+// TODO add comments
 
 // export our app
 export default app;
